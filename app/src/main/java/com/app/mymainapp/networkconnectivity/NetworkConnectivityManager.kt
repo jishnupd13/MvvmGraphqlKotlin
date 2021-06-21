cbc98d5e749d76c40e62b25b2b5639f163fdf994
jishnu <jishnu.webandcrafts@gmail.com>
2021-06-21 11:10:59 +0530

    network connectivity manager updated

---
+++ app/src/main/java/com/app/mymainapp/networkconnectivity/NetworkConnectivityManager.kt
@@ -0,0 +1,65 @@
+package com.app.mymainapp.networkconnectivity
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import com.app.mymainapp.utils.Constants
+import timber.log.Timber
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
+
+object NetworkConnectivityManager {
+
+    private fun hasNetworkAvailable(context: Context): Boolean {
+        var result = false
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            cm?.run {
+                cm.getNetworkCapabilities(cm.activeNetwork)?.run {
+                    result = when {
+                        hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                        hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                        hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                        else -> false
+                    }
+                }
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            cm?.run {
+                cm.activeNetworkInfo?.run {
+                    if (type == ConnectivityManager.TYPE_WIFI) {
+                        result = true
+                    } else if (type == ConnectivityManager.TYPE_MOBILE) {
+                        result = true
+                    }
+                }
+            }
+        }
+        return result
+    }
+
+
+    fun hasInternetConnected(context: Context): Boolean {
+        if (hasNetworkAvailable(context)) {
+            try {
+                val connection =
+                    URL(Constants.REACHABILITY_SERVER).openConnection() as HttpURLConnection
+                connection.setRequestProperty("User-Agent", "Test")
+                connection.setRequestProperty("Connection", "close")
+                connection.connectTimeout = 1500 // configurable
+                connection.connect()
+                Timber.d("hasInternetConnected: ${(connection.responseCode == 200)}")
+                return (connection.responseCode == 200)
+            } catch (e: IOException) {
+                Timber.e("Error checking internet connection")
+            }
+        } else {
+            Timber.d("No network available!")
+        }
+        Timber.d("hasInternetConnected: false")
+        return false
+    }
+}
--- app/src/main/java/com/app/mymainapp/utils/Constants.kt
+++ app/src/main/java/com/app/mymainapp/utils/Constants.kt
@@ -6,5 +6,6 @@ class Constants {
         const val BASE_URL = "https://jsonplaceholder.typicode.com/"
         const val SHARED_PREFERENCE_KEY="MyMainApp"
         const val ROOM_DATABASE_NAME="app_local_database"
+        const val REACHABILITY_SERVER="https://www.google.com"
     }
 }
